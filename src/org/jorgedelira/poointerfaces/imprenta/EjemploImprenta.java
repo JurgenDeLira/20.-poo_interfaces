84846632b11f682a73b0b441c09bfa57e490d603
JurgenDeLira <arq.jorgedelira@gmail.com>
2025-03-20 14:03:40 -0700

    Tercer commit

--- src/org/jorgedelira/poointerfaces/imprenta/EjemploImprenta.java
+++ src/org/jorgedelira/poointerfaces/imprenta/EjemploImprenta.java
@@ -6,21 +6,24 @@ import static org.jorgedelira.poointerfaces.imprenta.modelo.Genero.*;
 public class EjemploImprenta {
     public static void main(String[] args) {
 
-        Curriculo cv = new Curriculo("John Doe", "Ingeniero de sistemas", "Resumen laboral...");
-                cv.addExperiencia("Java");
-        cv.addExperiencia("Oracle DBA");
-        cv.addExperiencia("Spring Framework");
-        cv.addExperiencia("Desarrollador fullstack");
-        cv.addExperiencia("Angular");
+        Curriculo cv = new Curriculo(new Persona("John", "Doe"), "Ingeniero de sistemas", "Resumen laboral...");
+        cv.addExperiencia("Java")
+        .addExperiencia("Oracle DBA")
+        .addExperiencia("Spring Framework")
+        .addExperiencia("Desarrollador fullstack")
+        .addExperiencia("Angular");
 
-        Libro libro = new Libro("George Orwell", "1984", CIENCIAFICCION);
+        Libro libro = new Libro(new Persona("George", "Orwell"),
+                "1984",
+                CIENCIAFICCION);
         libro.addPagina(new Pagina("Patrón singleton"))
                 .addPagina(new Pagina("Patrón Observador"))
                 .addPagina(new Pagina("Patrón Factory"))
                 .addPagina(new Pagina("Patrón Composite"))
-                .addPagina(new Pagina("Patrón Faccade"));
+                .addPagina(new Pagina("Patrón Facade"));
 
-        Informe informe = new Informe( "Martín Fowler", "James", "Estudio sobre microservicios");
+        Informe informe = new Informe( new Persona("Martín", "Fowler"),
+                new Persona("James", "Gosling"), "Estudio sobre microservicios");
         imprimir(cv);
         imprimir(informe);
         imprimir(libro);
